f262943c811bd70515b8cb33236045c982409f19
ItsMikeyy <michaelcarey14@outlook.com>
2025-05-15 22:35:46 -0600

    ball movement

--- Scenes/Ball/ball.gd
+++ Scenes/Ball/ball.gd
@@ -4,16 +4,22 @@ class_name Ball
 
 @onready var ball_sprite: AnimatedSprite3D = $AnimatedSprite3D
 
-@export var power: Vector3 = Vector3(0.0, 0.0,-700)
-const MOVE_SPEED: float =  300
+const MOVE_SPEED: float = 300.0
 var is_moving: bool = true
 var is_aiming: bool = false
 var is_charging: bool = false
 var has_thrown: bool = false
 
+var control_bar: Node
+var power_bar: Node
+var power: float = 0
+var horizontal_power: float = 0
+
 func _ready() -> void:
 	ball_sprite.animation = "rest"
-	
+	control_bar = get_node("../../UI/MainGameUI/MarginContainer/VBoxContainer/ControlBar")
+	power_bar = get_node("../../UI/MainGameUI/MarginContainer/VBoxContainer/PowerBar")
+	print(control_bar)
 func _physics_process(delta: float) -> void:
 	
 	if is_moving:
@@ -33,22 +39,25 @@ func move_ball(delta: float) -> void:
 	if Input.is_action_just_pressed("CONFIRM"):
 		is_moving = false
 		is_aiming = true
-		SignalHandler.set_control_active_animation.emit()
+		control_bar.set_active_animation()
 		
 func aim_ball() -> void:
 	if Input.is_action_just_pressed("CONFIRM"):
 		is_aiming = false
 		is_charging = true
-		SignalHandler.pause_control_active_animation.emit()
-		SignalHandler.set_power_active_animation.emit()
+		horizontal_power = control_bar.pause_active_animation()
+		print(horizontal_power)
+		power_bar.set_active_animation()
 
 func charge_ball() -> void:
 	if Input.is_action_just_pressed("CONFIRM"):
 		is_charging = false
 		has_thrown = true
-		SignalHandler.pause_power_active_animation.emit()
-
+		power = power_bar.pause_active_animation()
+		power_bar.set_rest_animation()
+		control_bar.set_rest_animation()
+		
 func throw_ball(delta: float) -> void:
-	linear_velocity = power * delta
+	linear_velocity = Vector3(horizontal_power,0,power) * delta
 	ball_sprite.animation = "throw"
 	ball_sprite.play()
--- Scenes/ControlBar/ControlBar.gd
+++ Scenes/ControlBar/ControlBar.gd
@@ -2,24 +2,21 @@ extends Control
 
 @onready var control_bar_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-func _enter_tree() -> void:
-	SignalHandler.set_control_rest_animation.connect(_set_rest_animation)
-	SignalHandler.set_control_active_animation.connect(_set_active_animation)
-	SignalHandler.pause_control_active_animation.connect(_pause_active_animation)
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_set_rest_animation()
+	set_rest_animation()
 
 
-func _set_rest_animation() -> void:
+func set_rest_animation() -> void:
 	control_bar_sprite.animation = "rest"
 	control_bar_sprite.play()
 
-func _set_active_animation() -> void:
+func set_active_animation() -> void:
 	control_bar_sprite.animation = "active"
 	control_bar_sprite.play()
 
-func _pause_active_animation() -> int:
+func pause_active_animation() -> int:
 	const DIRECTION_SPEED: float = 10.0
 	control_bar_sprite.pause()
 	var frame: int = control_bar_sprite.frame
